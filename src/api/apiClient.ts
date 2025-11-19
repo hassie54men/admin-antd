79c9c42f9a9241d60465612c5ba5834fcedcf29a
hassie54men <hassie54men@icloud.com>
2025-11-19 23:34:20 +0300

    feat: install and configure Axios

---
+++ src/api/apiClient.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const baseUrl = import.meta.env.VITE_API_URL;
+
+const apiClient = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default apiClient;
