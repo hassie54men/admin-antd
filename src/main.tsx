724e81ca69c918dac0000f661fdfc3f25a179c31
hassie54men <hassie54men@icloud.com>
2025-11-20 23:18:19 +0300

    feat: install and configure react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <StrictMode>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <App />
     </StrictMode>
   </BrowserRouter>,
 );
