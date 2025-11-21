6d77d1d65d5c3d97488efa000e014a4933776ecd
hassie54men <hassie54men@icloud.com>
2025-11-21 17:58:11 +0300

    refactor: replace App with AppRoutes in main.tsx entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
+import "normalize.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
 import { BrowserRouter } from "react-router";
 import "./i18n";
+import AppRoutes from "./app/providers/AppRoutes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <AppRoutes />
     </StrictMode>
   </BrowserRouter>,
 );
