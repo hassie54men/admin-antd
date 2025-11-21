3fbdcae08d57fb1b5408fe2e62e6901dbbd2f4a1
hassie54men <hassie54men@icloud.com>
2025-11-21 12:06:09 +0300

    feat: install and configure i18n

---
+++ src/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import ru from "../public/locales/ru/translation.json";
+import en from "../public/locales/en/translation.json";
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: "en",
+    resources: {
+      ru: {
+        translation: ru,
+      },
+      en: {
+        translation: en,
+      },
+    },
+  });
+
+export default i18n;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App.tsx";
 import { BrowserRouter } from "react-router";
+import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
