f2786e1cec67b0c7f3633e33e31cf48400c5f020
hassie54men <hassie54men@icloud.com>
2025-11-24 23:23:37 +0300

    feat: create authlayout , login, home component and added them in approutes

--- src/app/providers/AppRoutes.tsx
+++ src/app/providers/AppRoutes.tsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router";
 import App from "../App";
+import AuthLayout from "../layout/AuthLayout.tsx";
+import Login from "../../features/auth/pages/Login.tsx";
+import Home from "../layout/Home.tsx";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<App />}>
-        <Route index element={<div>HOME</div>} />
-        <Route element={<div>AUTH-LAYOUT</div>}>
-          <Route path="login" element={<div>LOGIN</div>} />
-          <Route path="register" element={<div>REGISTER</div>} />
+        <Route index element={<Home />} />
+        <Route element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
         </Route>
         <Route path="*" element={<div>NotFound</div>} />
       </Route>
