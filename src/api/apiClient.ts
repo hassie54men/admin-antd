0c93654df1a078bd8e2d1996510ebeebb9f9d0a4
hassie54men <hassie54men@icloud.com>
2025-11-26 22:07:23 +0300

    feat: added token handling in apiClient

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -9,4 +9,18 @@ const apiClient = axios.create({
   },
 });
 
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      // в TS желательно проверять headers
+      config.headers = config.headers ?? {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 export default apiClient;
