fc7ad31a6dd899f913e803b51a195b0fc9d99e49
hassie54men <hassie54men@icloud.com>
2025-11-26 22:09:05 +0300

    refactor: update Home component

--- src/app/layout/Home.tsx
+++ src/app/layout/Home.tsx
@@ -1,11 +1,9 @@
-import { useNavigate } from "react-router";
 import LoginButton from "../../features/auth/ui/LoginButton.tsx";
 
 const Home = () => {
-  const navigate = useNavigate();
   return (
     <div>
-      <LoginButton onClick={() => navigate("/login")}>login</LoginButton>
+      <LoginButton />
     </div>
   );
 };
