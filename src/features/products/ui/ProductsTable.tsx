70316f0361cf29885a28d7422218a9c39540ea9a
hassie54men <hassie54men@icloud.com>
2025-12-06 23:21:00 +0300

    refactor: update language files and add translate in products table component

--- src/features/products/ui/ProductsTable.tsx
+++ src/features/products/ui/ProductsTable.tsx
@@ -1,38 +1,57 @@
 import { Table, Typography } from "antd";
 import { useGetProductsQuery } from "../model/product.queries.ts";
 import type { Product } from "../model/product.types.ts";
-
-const colums = [
-  {
-    title: "Title",
-    dataIndex: "title",
-    key: "title",
-    render: (text: string) => (
-      <Typography.Text copyable>{text}</Typography.Text>
-    ),
-  },
-  {
-    title: "Category",
-    dataIndex: "category",
-    key: "category",
-  },
-  {
-    title: "Price",
-    dataIndex: "price",
-    key: "price",
-    sorter: (a: Product, b: Product) => a.price - b.price,
-  },
-  {
-    title: "Rating",
-    dataIndex: "rating",
-    key: "rating",
-    sorter: (a: Product, b: Product) => a.rating - b.rating,
-  },
-];
+import { useTranslation } from "react-i18next";
 
 const ProductsTable = () => {
-  const { data } = useGetProductsQuery();
-  return <Table<Product> dataSource={data} columns={colums} />;
+  const { t } = useTranslation();
+  const { data, isLoading, isError } = useGetProductsQuery();
+
+  if (isError) {
+    return <div>{t("table.error")}</div>;
+  }
+
+  const colums = [
+    {
+      title: t("table.id"),
+      dataIndex: "id",
+      key: "id",
+      render: (text: number) => <a>{text}</a>,
+    },
+    {
+      title: t("table.title"),
+      dataIndex: "title",
+      key: "title",
+      render: (text: string) => (
+        <Typography.Text copyable>{text}</Typography.Text>
+      ),
+    },
+    {
+      title: t("table.category"),
+      dataIndex: "category",
+      key: "category",
+    },
+    {
+      title: t("table.price"),
+      dataIndex: "price",
+      key: "price",
+      sorter: (a: Product, b: Product) => a.price - b.price,
+    },
+    {
+      title: t("table.rating"),
+      dataIndex: "rating",
+      key: "rating",
+      sorter: (a: Product, b: Product) => a.rating - b.rating,
+    },
+  ];
+
+  return (
+    <Table<Product>
+      dataSource={data?.products}
+      columns={colums}
+      loading={isLoading}
+    />
+  );
 };
 
 export default ProductsTable;
