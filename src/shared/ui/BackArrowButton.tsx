e7b65e0b17a2e9438aaa6372e3a5afcd018907d7
hassie54men <hassie54men@icloud.com>
2025-12-13 22:51:49 +0300

    feat: create back arrow button

---
+++ src/shared/ui/BackArrowButton.tsx
@@ -0,0 +1,22 @@
+import { Button } from "antd";
+import { useNavigate } from "react-router";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+
+const BackArrowButton = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Button
+      type="text"
+      onClick={() => navigate(-1)}
+      icon={<ArrowLeftOutlined style={{ color: "black", fontSize: 18 }} />}
+      style={{
+        padding: 0,
+        width: 32,
+        height: 32,
+      }}
+    />
+  );
+};
+
+export default BackArrowButton;
